02ed7daa495643f0d847276da34dfaaa14ee69be
Louis-Olivier Brassard <louis@loupbrun.ca>
2019-05-14 23:04:31 -0400

    Title: slash separator instead of hyphen `-`

--- view.php
+++ view.php
@@ -3,7 +3,7 @@
 	<head>
 
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-		<title>Louis-Olivier Brassard – Albums virtuels</title>
+		<title>Louis-Olivier Brassard / Albums web</title>
 
 		<meta name="author" content="Tobias Reich">
 
